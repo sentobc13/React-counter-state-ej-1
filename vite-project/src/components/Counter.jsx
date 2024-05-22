4f62acbc90e8574cbd2123d62a957e2ed78443a7
sentobc13 <sentoberlanga@gmail.com>
2024-05-22 12:12:45 +0200

    contador funciona

---
+++ vite-project/src/components/Counter.jsx
@@ -0,0 +1,21 @@
+
+import { useState } from "react";
+
+const Counter = ({initialValue,suma}) => {
+    const [counter, setCounter] = useState(initialValue);
+    const increment = () => {
+        setCounter(counter + suma);
+    };
+    const decrement = () => {
+        setCounter(counter - suma);
+    };
+    return <div>
+        <button onClick={decrement}> - </button>
+        <span> {counter} </span>
+        <button onClick={increment}> + </button>
+
+    </div>;
+}
+
+
+export default Counter
